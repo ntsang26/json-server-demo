aba870e18ab56f23500bea809ed03e6f69cfe6b1
Nguyễn Thanh Sang <ntsang26@github.com>
2021-07-13 11:10:05 +0700

    update custom output

--- main.js
+++ main.js
@@ -1,30 +1,53 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const queryString = require("query-string");
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
+server.use(middlewares);
 
 // Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
   }
   // Continue to JSON Server router
-  next()
-})
+  next();
+});
 
-// Use default router 
-server.use("/api", router)
-const PORT = process.env.PORT || 3000;
+router.render = (req, res) => {
+  const headers = res.getHeaders();
+
+  const totalCountHeader = headers["x-total-count"];
+  if (req.method === "GET" && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
+    console.log(queryParams);
+
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _page: Number.parseInt(queryParams._page) || 1,
+        _totalRows: Number.parseInt(totalCountHeader),
+      },
+    };
+    return res.jsonp(result);
+  }
+
+  res.jsonp({ data: res.locals.data });
+};
+
+// Use default router
+server.use("/api", router);
+const PORT = process.env.PORT || 8888;
 server.listen(PORT, () => {
-  console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
